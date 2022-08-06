31ed5e4da5259d02d1b46df5d7dda7ec7c9a011a
sanjeevk <sanjeevk@gnrgy.com>
2022-08-06 14:28:53 +0530

    Saving changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
-import { AppProvider } from './components/AppContext';
-import Navbar from './components/Navbar';
+import Form from './components/Form';
 
 
 function App() {
-  const userData = {
-    name: "Sanjeev",
-    sex : "Male"
-  }
-  const name1 = userData.name;
-  const name2 = userData["name"]
-  const {name} = userData;
-  console.log(name2)
+
   return (
-    <AppProvider>
-      <Navbar userData={userData} />
-    </AppProvider>
+   <Form/>
   );
 }
 
