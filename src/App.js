272e7ca33e198ca866f5e6cb2f1375584738d1bf
sanjeevk <sanjeevk@gnrgy.com>
2022-08-07 22:13:41 +0530

    useState taught in this commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-
-import './App.css';
-import CreditCardForm from './components/CreditCardForm';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [count,setCount] = useState(0);
+  //Hooks : Nothing  but a function
+  //useStateHook adds a local state to a functional component
+  //State is nothing but a variable to store data and preserve it during re-render
+  //So useState is also a function(hook) which takes one parameter which is 
+  //nothing but intial state
 
-//Commented for demo
+  //When you call this hook(function) then it returns us two things
+
+  //1. State(nothing but a variable to store data) name and count are state here for example
+
+  //2. It returns an another function to update those states(name , count)
+
+  //Whenever state changes component will re-render
+
+  console.log("Count", count)
+  console.log("Name",name)
   return (
     <div className="App">
-       <CreditCardForm />
+      <input
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter Name"
+      />
+      <button onClick={()=> setCount(count+1)}>Increase</button>
     </div>
   );
 }
