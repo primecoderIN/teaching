ff06885413fdea79f9f09c2debb035a6db2d457f
sanjeevk <sanjeevk@gnrgy.com>
2022-08-07 22:35:22 +0530

    taught radio button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-
-import './App.css';
-import CreditCardForm from './components/CreditCardForm';
-
+import { useState } from "react";
+import "./App.css";
 function App() {
+  const [gender, setGender] = useState("Other");
+  console.log("gender", gender);
+  //Facts
 
-
+  //1. Name property on all radio button will be same
+  //2/ Value prop on input element will be hardcoded
+  //Checked prop is used to indicate user which option selected
   return (
     <div className="App">
-       <CreditCardForm />
+      <div className="item">
+        <label htmlFor="male">Male</label>
+        <input
+          id="male"
+          type="radio"
+          name="gender"
+          value="Male"
+          onChange={(e) => setGender(e.target.value)}
+          checked={gender === "Male"}
+        />
+      </div>
+
+      <div className="item">
+        <label htmlFor="female">Female</label>
+        <input
+          id="female"
+          type="radio"
+          name="gender"
+          value="Female"
+          onChange={(e) => setGender(e.target.value)}
+          checked={gender === "Female"}
+        />
+      </div>
+
+      <div className="item">
+        <label htmlFor="other">Other</label>
+        <input
+          id="other"
+          type="radio"
+          name="gender"
+          value="Other"
+          onChange={(e) => setGender(e.target.value)}
+          checked={gender === "Other"}
+        />
+      </div>
     </div>
   );
 }
