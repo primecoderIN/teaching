81911ee635c9bce564bb28e6af8f7e287f46eb7e
sanjeevk <sanjeevk@gnrgy.com>
2022-08-20 12:06:09 +0530

    taught checkbox controlled input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
+import {useState} from "react"
+const CheckBoxGroup = ()  => {
 
-import './App.css';
-import CreditCardForm from './components/CreditCardForm';
-
-function App() {
-
-
+  const [gender,setGender] = useState("Female")
+const handleChange = (e) => {
+  setGender(e.target.value)
+}
   return (
-    <div className="App">
-       <CreditCardForm />
+    <div>
+      <h4> Select gender </h4>
+      <div>
+        <input
+        id='male'
+          value="Male"
+          type="checkbox"
+          checked={gender === "Male"}
+          onChange={handleChange}
+        />
+        <label htmlFor="male">Male</label>
+      </div>
+
+      <div>
+        <input
+          id='female'
+          value="Female"
+          type="checkbox"
+          checked={gender === "Female"}
+          onChange={handleChange}
+        />
+        <label htmlFor='female'>Female</label>
+      </div>
+
+      <div>
+        <input
+        id='other'
+          type="checkbox"
+          value="Other"
+          checked={gender === "Other"}
+          onChange={handleChange}
+        />
+        <label htmlFor="other">Other</label>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default CheckBoxGroup;
